0b5d6eba36332155d7b18257bd9b46c9a96db417
Vleva1 <supermolotqwe@gmail.com>
2023-05-12 19:11:02 +0300

    secret key is hidden

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ei=oe3fhq64e78u-w^7^z4yf0ultmdo)4zasufe#is9gpy3a^'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
